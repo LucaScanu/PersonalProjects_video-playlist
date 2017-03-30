43b42b89dc97b83db8e5e8b1b319e1e4437f9e5f
jackhkmatthews <jackhkmatthews@gmail.com>
2017-03-30 12:40:32 +0100

    gulp setup copied from a previous project and then adjusted to fit. Also changes server.js to look at new public directory for static files and index.html

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const config        = require('./config/config');
 
 const app           = express();
 
-app.use('/', express.static(path.join(__dirname, '/')));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
+app.get('/', (req, res) => res.sendFile(`${__dirname}/publick/index.html`));
 
 app.listen(config.port, () => console.log(`listening on ${config.port}`));
