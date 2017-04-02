2ca9f779d893ec817ab18999a16302f55e534195
jackhkmatthews <jackhkmatthews@gmail.com>
2017-04-02 16:34:27 +0100

    edited server.js file to prevent reserving of index.html

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const dest          = `${__dirname}/public`;
 
 const app           = express();
 
-app.use('/', express.static(dest));
-app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
+// app.use('/', express.static(dest));
+// app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
+
+app.use('/', express.static('public'));
+app.use('/', express.static('bower_components'));
+
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.listen(config.port, () => console.log(`listening on ${config.port}`));
